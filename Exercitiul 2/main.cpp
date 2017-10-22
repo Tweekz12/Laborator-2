f3a67ca9bf2825a7c66714f5123717a8c17b31fd
Tweekz12 <32911560+Tweekz12@users.noreply.github.com>
2017-10-22 21:55:32 +0300

    Add files via upload

---
+++ Exercitiul 2/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    double x;
+    cout << "x=";
+    cin >> x;
+    cout << "Modulul lui x este: " << (x*-1) << endl;
+    return 0;
+}
