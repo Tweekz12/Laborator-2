3a5f2ec1334b706c8cf21cf15ab22d2629448618
Tweekz12 <32911560+Tweekz12@users.noreply.github.com>
2017-10-22 19:30:53 +0300

    Add files via upload

---
+++ Exercitiul 5/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <conio.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+ int a,b,c;
+ cout << "a=";
+ cin >> a;
+ cout << "b=";
+ cin >> b;
+ cout << "c=";
+ cin >> c;
+ if (a>b && a>c && a!=c && b!=c) {
+    if ((a*a==(b*b)+(c*c)))
+        cout << "Triunghiul este dreptunghic." << endl;
+    else
+        cout << "Triunghiul este oarecare." << endl;
+ }
+  if (b>a && b>c && a!=c && b!=c) {
+    if ((b*b==(a*a)+(c*c)))
+        cout << "Triunghiul este dreptunghic." << endl;
+    else
+        cout << "Triunghiul este oarecare." << endl;
+ }
+ if (c>a && c>b && b!=a && c!=a) {
+    if ((c*c==(a*a)+(b*b)))
+        cout << "Triunghiul este dreptunghic." << endl;
+    else
+        cout << "Triunghiul este oarecare." << endl;
+ }
+ if (a==b && a==c && b==c)
+    cout << "Triunghiul este echilateral." << endl;
+ if (a==b && a!=c && b!=c)
+    cout << "Triunghiul este isoscel." << endl;
+ if (a==c && a!=b && c!=b)
+    cout << "Triunghiul este isoscel." << endl;
+ if (b==c && b!=a && c!=a)
+    cout << "Triunghiul este isoscel." << endl;
+return 0;
+ }
+
